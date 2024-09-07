577f4210ea49bb78cded3b75dfcd01074dc19cbf
Екатерина <pygavko@yandex.ru>
2024-09-07 22:10:25 +0300

    Исправить ошибки в стиле написания кода в классе Node

--- src/manager/Node.java
+++ src/manager/Node.java
@@ -1,13 +1,13 @@
 package manager;
 
-public class Node <T> {
+public class Node<T> {
     public T data;
     public Node<T> next;
     public Node<T> previous;
 
-    public Node(Node<T> previous, T data, Node <T> next) {
+    public Node(Node<T> previous, T data, Node<T> next) {
         this.data = data;
-        this.next = next ;
+        this.next = next;
         this.previous = previous;
     }
 }
